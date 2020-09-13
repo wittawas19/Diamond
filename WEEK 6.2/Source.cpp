e08c7fe3d6ace78a238014241ca146158ad41a6e
wittawas19@gmail.com <wittawas19@gmail.com>
2020-09-13 18:11:35 +0700

    Add project files.

---
+++ WEEK 6.2/Source.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNING
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = (n - 1); j > i; j--)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k <= 2 * (i); k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k <= i ; k++)
+		{
+			printf(" ");
+		}
+		for (int j = (n - 1); j >= 2 * i; j--)
+		{
+			printf("*");
+		}
+		printf("\n"); 
+
+	}
+
+}
